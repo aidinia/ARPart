5a8b95f90be8b62bff143c897f3b32e1abb7b104
Aida <1708047@rgu.ac.uk>
2021-02-16 15:35:21 +0000

    Merge with network
    Merge both projects and initiate camera as player?

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+
+    public TextMesh text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var thisPlayer = PhotonNetwork.Instantiate("AR Camera", Vector3.zero,Quaternion.identity);
+        text.text = "Player initiated";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerIcon.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerIcon : MonoBehaviour
+{
+    Object[] guids2;
+    public GameObject playerIcon;
+    public Text text;
+    int index = 0;
+    bool touching = false;
+
+    void Awake()
+    {
+        // Load icons
+        guids2 = Resources.LoadAll("ARISAN/100 Alchemy item icons-free", typeof(Sprite));
+    }
+
+ 
+    public void changeIcon()
+    {
+        index++;
+
+        if (index == guids2.Length)
+        {
+            index = 0;
+        }
+        var icon = guids2[index];
+        playerIcon.GetComponent<Image>().sprite = (Sprite)icon;
+    }
+}
